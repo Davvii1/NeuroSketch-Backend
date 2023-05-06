a85ae6fec5da96eeac653df544065540eda818e8
Davvii1 <david2002rg@hotmail.com>
2023-05-06 14:54:58 -0600

    finish

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import multer from "multer";
+import bodyParser from "body-parser";
 
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js'
-import dalleRoutes from './routes/dalleRoutes.js'
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
+const multerMid = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.disable("x-powered-by");
+app.use(multerMid.single("file"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/v1/post', postRoutes)
-app.use('/api/v1/dalle', dalleRoutes)
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+app.use("/api/v1", authRoutes);
 
-app.get('/', async (req, res) => {
-    res.send('Hello from DALL-E');
-})
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E");
+});
 
 const startServer = async () => {
-
-    try {
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
-    } catch (error) {
-        console.log(error);
-    }
-    
-    
-}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 startServer();
---
+++ mongodb/models/user.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+import Joi from "joi";
+import passwordComplexity from "joi-password-complexity";
+
+dotenv.config();
+
+const userSchema = new mongoose.Schema({
+  nickname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, select: false, required: true },
+  images: [{ type: { id: String, url: String }, required: false }],
+  refreshToken: { type: String, select: false, required: false },
+});
+
+userSchema.methods.generateAuthToken = function (user) {
+  const token = jwt.sign({ user: user.email }, process.env.JWTPRIVATEKEY, {
+    expiresIn: "1h",
+  });
+  return token;
+};
+
+userSchema.methods.generateRefreshToken = function (user) {
+  const refreshToken = jwt.sign(
+    { user: user.email },
+    process.env.JWTREFRESHKEY
+  );
+  return refreshToken;
+};
+
+// Middleware
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401); //.send({ message: "Not valid token" });
+  jwt.verify(token, process.env.JWTPRIVATEKEY, (err, user) => {
+    if (err) return res.sendStatus(403); //.send({ message: "Expired token" });
+    req.user = user;
+  });
+  next();
+};
+
+export const validateRegister = (data) => {
+  const schema = Joi.object({
+    nickname: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: passwordComplexity().required(),
+  });
+  return schema.validate(data);
+};
+
+export const validateLogin = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().label("Email"),
+    password: Joi.string().required().label("Password"),
+  });
+  return schema.validate(data);
+};
+
+export const validateUpdate = (data) => {
+  const schema = Joi.object({
+    nickname: Joi.string().required(),
+    email: Joi.string().email().required().label("Email"),
+  });
+  return schema.validate(data);
+};
+
+export const validatePassword = (data) => {
+  const schema = Joi.object({
+    password: Joi.string().required().label("Password"),
+  });
+  return schema.validate(data);
+};
+
+export const User = mongoose.model("user", userSchema);
